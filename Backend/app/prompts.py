873ec8c79a3d6c8e06b66112ac88c9c4fab928ba
Khaerul743 <khaerullutfi028@gmail.com>
2025-08-14 21:10:09 +0700

    setup prompt

--- Backend/app/prompts.py
+++ Backend/app/prompts.py
@@ -1,16 +1,22 @@
+from typing import Optional
+
 from langchain_core.messages import HumanMessage, SystemMessage
+from memory import MemoryControl
 
 
 class AgentPromptControl:
-    def __init__(self, memory_provider: str, provider_host: str, provider_port: str):
-        if not memory_provider or not provider_host or not provider_port:
-            raise ValueError("All field is required!")
-        
-        self.config = {
-            "vector_store": {
-                "provider": memory_provider,
-                "config": {"host": provider_host, "port": provider_port},
-            }
-        }
-
-        self.memory = 
+    def __init__(
+        self,
+        is_include_memory: bool = False,
+        memory_provider: Optional[str] = "",
+        provider_host: Optional[str] = "",
+        provider_port: Optional[str] = "",
+    ):
+        self.is_include_memory = is_include_memory
+        if self.is_include_memory:
+            self.memory = MemoryControl(
+                memory_provider=memory_provider,
+                provider_host=provider_host,
+                provider_port=provider_port,
+            )
+    
